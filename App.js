c87023f275245703f326de381e66a3a433f91a4c
adanbae <adan@adanui-MacBookPro.local>
2020-01-09 09:27:56 +0900

    CHANGE - 5.3 프로파일 카드 컴포넌트를 원래 크기에서 섬네일 크기로 축소 ;

--- App.js
+++ App.js
@@ -1,31 +1,89 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, Image, Platform } from 'react-native'
+import { StyleSheet, View, Text, Image, Platform, TouchableHighlight } from 'react-native'
+import PropTypes from 'prop-types'
+import update from 'immutability-helper'
 
+
+const userImage = require('./user.png')
+
+const data = [{
+  image: userImage,
+  name: 'John Doe',
+  occupation: 'React Native Devloper',
+  description: 'John is a really great JavaScript developer, He Loves using JS to build React Native applications for iOS and Android.',
+  showThumbnail: true
+}];
+
+const ProfileCard = (props) => {
+  const { image, name, occupation, description, onPress, showThumbnail } = props;
+  let conatinerStyls = [styles.cardContainer];
+
+  if(showThumbnail){
+    conatinerStyls.push(styles.cardThumbnail)
+  }
+  return (
+    <TouchableHighlight onPress= {onPress} >
+      <View style={[conatinerStyls]}>
+        <View style={styles.cardImageContainer}>
+          <Image style={styles.cardImage} source={image} />
+        </View>
+        <View>
+          <Text style={styles.cardTitle}>
+            {name}
+          </Text>
+        </View>
+        <View style={styles.cardSubtitleContainer}>
+          <Text style={styles.cardSubtitle}>
+            {occupation}
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.cardDescription}>
+            {description}
+          </Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  )
+}
+
+ProfileCard.propTypes = {
+  image: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequiredm,
+  occupation : PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  showThumbnail: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired
+}
 export default class App extends Component {
+
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      data: data
+    }
+  }
+
+  handleProfileCardPress = (index) => {
+    const showThumbnail = !this.state.data[index].showThumbnail
+    this.setState({
+      data: update(this.state.data, {[index]:{showThumbnail: {$set: showThumbnail}}})
+    })
+  }
   render() {
+    const list = this.state.data.map(function(item, index) {
+      const { image, name, occupation, description, showThumbnail } = item
+      return <ProfileCard key={'card-'+index}
+                          image={image}
+                          name={name}
+                          occupation={occupation}
+                          description={description}
+                          onPress={this.handleProfileCardPress.bind(this, index)}
+                          showThumbnail={showThumbnail}/>
+    }, this)
     return (
       <View style={styles.container}>
-        <View style={styles.cardContainer}>
-          <View style={styles.cardImageContainer}>
-            <Image style={styles.cardImage} source={require('./user.png')} />
-          </View>
-          <View>
-            <Text style={styles.cardTitle}>
-              John Doe
-            </Text>
-          </View>
-          <View style={styles.cardSubtitleContainer}>
-            <Text style={styles.cardSubtitle}>
-              React Native Developer
-            </Text>
-          </View>
-          <View>
-            <Text style={styles.cardDescription}>
-              John is a really great JavaScript developer, He Loves using JS to build React Native applications for iOS and Android.
-
-            </Text>
-          </View>
-        </View>
+        {list}
       </View>
     )
   }
@@ -37,8 +95,11 @@ const styles = StyleSheet.create({
   container : {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
 
+  },
+  cardThumbnail: {
+    transform:[{scale: 0.2}]
   },
   cardContainer: {
     alignItems: 'center',
